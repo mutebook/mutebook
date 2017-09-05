372a7659f45db4c8523ec953cd76f8eaf250d265
Jan Burle <jan@c53.be>
2017-09-05 11:07:45 +0200

    todo:, image captions, und mehr

---
+++ hook.js
@@ -0,0 +1,6 @@
+/* global CM:false book:false */
+book.hook = function (/* tag, cs, parts */) {
+  return false;
+};
+
+// TODO handle loading error if this file does not exist
--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
-$book = [
-  'title'   => 'Mutebook',
-  'banner'  => 'Mutebook',
-  'pages'   => 'pg/',
+const META_DESCRIPTION =
+  'Online interactive textbook to learn and teach music technology and music production.';
+const META_KEYWORDS =
+  'Music technology, online teaching, music production, acoustics, electronics, digital audio';
+const META_AUTHOR =
+  'Thilo Schaller & Jan Burle';
 
-  'sc_project'  => 11395717,
-  'sc_security' => '03ed7177',
+const PAGES = 'pg/'; $pages = PAGES;
 
-  'hooks' => function () {}
-];
+$book = <<<EOT
+  title:  'Mutebook', banner: 'Mutebook', pages: '$pages',
+  sc_project: 11395717, sc_security: '03ed7177',
+EOT;
 
 require(dirname(__FILE__).'/CM/_index.php');
 
