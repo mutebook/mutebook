afb8bf057a61c2160518c7fc0836d9f108bdfcf2
Jan Burle <jan@c53.be>
2017-09-10 14:41:39 +0200

    ..

--- MC/utils.js
+++ MC/utils.js
@@ -10,13 +10,14 @@ MC.Utils = class {
       // TODO
       const par = og.gai._node.gain;
       par.setValueAtTime(0, time);
-      par.linearRampToValueAtTime(.2, time + 2);
-      og.safeStart(time + .1);
+      par.linearRampToValueAtTime(.2, time + .6);
+      og.safeStart(time);
     } else {
-      const og = owner.osc; delete owner.osc;
+      const og = owner.osc; owner.osc = null;
       // TODO
       const par = og.gai._node.gain;
-      par.exponentialRampToValueAtTime(.0001, time += .3);
+      par.cancelScheduledValues(time);
+      par.exponentialRampToValueAtTime(.0001, time += .6);
       par.setValueAtTime(0, time += .1);
       MC.Async.after(1, () => {
         og.safeStop();
