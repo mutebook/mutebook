2b73361bc4a4b1b0d7f06968264944563eff4918
Jan Burle <jan@c53.be>
2017-11-02 15:09:00 +0100

    removed compileToc

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
+$book = [
+  'title'       => 'Mutebook',
+  'banner'      => 'Mutebook',
 
-const META_DESCRIPTION =
-  'Online interactive textbook to learn and teach music technology and music production.';
-const META_KEYWORDS =
-  'Music technology, online teaching, music production, acoustics, electronics, digital audio';
-const META_AUTHOR =
-  'Thilo Schaller & Jan Burle';
+  'description' => 'Online interactive textbook to learn and teach music technology and music production.',
+  'keywords'    => 'Music technology, online teaching, music production, acoustics, electronics, digital audio',
+  'author'      => 'Thilo Schaller & Jan Burle',
 
-const PAGES = 'pg/'; $pages = PAGES;
+  'pages'       => 'pg/',
 
-$book = <<<EOT
-  title:  'Mutebook', banner: 'Mutebook', pages: '$pages',
-  sc_project: 11395717, sc_security: '03ed7177',
-EOT;
+  'sc_project'  => '11395717',
+  'sc_security' => '03ed7177',
+];
 
 require(dirname(__FILE__).'/CM/_index.php');
 
